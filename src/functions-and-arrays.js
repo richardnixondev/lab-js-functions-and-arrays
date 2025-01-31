02d7874547f23a0d42ade2d8f71ab83be4d723bd
Richard Nixon <rnixonaf@gmail.com>
2025-01-31 23:46:55 +0000

    completed the iterations 1,2,3,4,5

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,5 +1,16 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(num1, num2) {
+    let bigNumber;
+    if (num1 > num2){
+        return num1;
+    }else if (num2 > num1){
+        return num2;
+    }else if (num1 === num2){
+        return num1, num2;
+    }else{
+        console.log("not found");
+    }
+}
 
 
 
@@ -7,7 +18,13 @@ function maxOfTwoNumbers() {}
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
+function findLongestWord(arr) {
+    if (arr.length === 0){
+        return null;
+    };
+    return arr.reduce((longest, word) =>
+        word.length > longest.length ? word : longest, "");
+};
 
 
 
@@ -15,7 +32,18 @@ function findLongestWord() {}
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(arr1) {
+    if (arr1.length === 0) { 
+        return 0;
+    }
+
+    let sumNumb = 0;
+    for (let i = 0; i < arr1.length; i++) {
+        sumNumb += arr1[i]; 
+    }
+
+    return sumNumb;
+}
 
 
 
@@ -23,12 +51,31 @@ function sumNumbers() {}
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
-
+function averageNumbers(med) {
+    if (med.length === 0){
+        return 0;}
+    let sumMed = 0;
+    for (let i = 0; i < med.length; i++) {
+      sumMed += med[i];
+    }
+    let medFinal = sumMed / med.length;
+    return medFinal;
+}
 
 
 
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(arr2, wordSearch) {
+    let result = arr2.filter((e) => e.startsWith(wordSearch));
+    if (arr2.length == []){
+        return null;
+    } else if (result.length > 0){
+        return true;
+    } else if (result.length == 0) {
+        return false;
+    } else {
+        console.log("no conditions met")
+    }
+}
